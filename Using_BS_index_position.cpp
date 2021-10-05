a6165ce08ad3365873c7c5233a36422c41b89b1f
Arunabh Bora <57027544+arunabh-alt@users.noreply.github.com>
2021-10-05 13:36:53 +0530

    Add files via upload

---
+++ Using_BS_index_position.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, arr[10], num, first, last, middle;
+    cout<<"Enter 4 Elements (in ascending order): ";
+    for(i=0; i<4; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to be Search: ";
+    cin>>num;
+    first = 0;
+    last = 3;
+    middle = (first+last)/2;
+    while(first <= last)
+    {
+        if(arr[middle]<num)
+            first = middle+1;
+        else if(arr[middle]==num)
+        {
+            cout<<"\nThe number, "<<num<<" found at Position "<<middle+1;
+            break;
+        }
+        else
+            last = middle-1;
+        middle = (first+last)/2;
+    }
+    if(first>last)
+        cout<<"1";
+    cout<<endl;
+    return 0;
+}
