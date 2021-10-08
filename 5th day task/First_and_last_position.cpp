865d1ce40582a77748388d8778fb4b54f34e6852
Arunabh Bora <57027544+arunabh-alt@users.noreply.github.com>
2021-10-08 22:19:26 +0530

    Update First_and_last_position.cpp

--- 5th day task/First_and_last_position.cpp
+++ 5th day task/First_and_last_position.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
+/*
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1] */
 int firstOc(int arr[], int low, int high, int x, int n)
 {
+
     if (high >= low) {
         int mid = low + (high - low) / 2;
         if ((mid == 0 || x > arr[mid - 1]) && arr[mid] == x)
@@ -17,6 +37,7 @@ int firstOc(int arr[], int low, int high, int x, int n)
 
 int lastOc(int arr[], int low, int high, int x, int n)
 {
+
     if (high >= low) {
         int mid = low + (high - low) / 2;
         if ((mid == n - 1 || x < arr[mid + 1]) && arr[mid] == x)
@@ -31,14 +52,20 @@ int lastOc(int arr[], int low, int high, int x, int n)
 
 int main()
 {
-    int arr[] = { 5,7,7,8,8,10 };
+    int arr[100];
     int n = sizeof(arr) / sizeof(int);
-
-    int x;
+    int x,i;
+    cout<<"Enter the array size: \n";
+    cin>>n;
+    cout<<"Enter array elements: \n";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
     cout<<"Enter the target value: \n";
     cin>>x;
-    printf("First Occurrence = %d\t",firstOc(arr, 0, n - 1, x, n));
-    printf("\nLast Occurrence = %d\n",lastOc(arr, 0, n - 1, x, n));
+    int a= firstOc(arr,0, n - 1, x, n);
+    int b= lastOc(arr, 0,n - 1, x, n);
+    cout<<a<<" "<<b<<endl;
 
     return 0;
 }
