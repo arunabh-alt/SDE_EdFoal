6ce10d360f32a56466564dda1a255cdbcf8355c1
Arunabh Bora <57027544+arunabh-alt@users.noreply.github.com>
2021-10-12 00:01:00 +0530

    Add files via upload

---
+++ 8th Day/Rotate_2D_array_90_degree.cpp
@@ -0,0 +1,75 @@
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+Example 3:
+Input: matrix = [[1]]
+Output: [[1]]
+
+Example 4:
+Input: matrix = [[1,2],[3,4]]
+Output: [[3,1],[4,2]]*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void display(int a[100][100],int);
+void rotateMatrix(int a[100][100],int );
+void display(int a[100][100],int n){
+            int l,m;
+             for(l=0;l<n;l++){
+                for(m=0;m<n;m++){
+                cout<<a[l][m]<<"  ";
+             }
+             cout<<endl;
+        }
+
+}
+void rotateMatrix(int a[100][100],int n)
+{
+    int i,j;
+    for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                int temp = a[j][i];
+                a[j][i] = a[i][j];
+                a[i][j] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int f = 0;
+            int en = n-1;
+            while(f<en){
+                int temp = a[i][en];
+                a[i][en] = a[i][f];
+                a[i][f] = temp;
+                f++;
+                en--;
+            }
+        }
+}
+
+int main(){
+        int a[100][100],i,j,n;
+        cout<<"Enter the number of rows and coloumns : \n";
+        cin>>n;
+        cout<<"Enter the matrix: \n";
+        for(i=0;i<n;i++){
+             for(j=0;j<n;j++){
+                cin>>a[i][j];
+             }
+        }
+        cout<<"The given matrix is: \n";
+        display(a,n);
+        cout<<"After rotation 90 Degree: \n";
+        rotateMatrix(a,n);
+        display(a,n);
+        return 0;
+
+}
