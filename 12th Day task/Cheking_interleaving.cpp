cac01dec1f673583c40ad1393d02fca5c6a9fe3e
Arunabh Bora <57027544+arunabh-alt@users.noreply.github.com>
2021-10-16 19:41:58 +0530

    Update Cheking_interleaving.cpp

--- 12th Day task/Cheking_interleaving.cpp
+++ 12th Day task/Cheking_interleaving.cpp
@@ -1,22 +1,15 @@
 /*Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
-
 An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
-
 s = s1 + s2 + ... + sn
 t = t1 + t2 + ... + tm
 |n - m| <= 1
 The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
 Note: a + b is the concatenation of strings a and b.
-
-
 Example:
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
 Output: true
-
-
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
-
 Input: s1 = "", s2 = "", s3 = ""
 Output: true*/
 #include <iostream>
@@ -43,16 +36,13 @@ bool CheckingInterleaving(char* s1, char* s2, char* s3)
                     IL[i][j] = IL[i - 1][j];
             }
             else if (
-                s1[i - 1] == s3[i + j - 1]
-                && s2[j - 1] != s3[i + j - 1])
+                s1[i - 1] == s3[i + j - 1]&& s2[j - 1] != s3[i + j - 1])
                 IL[i][j] = IL[i - 1][j];
             else if (
-                s1[i - 1] != s3[i + j - 1]
-                && s2[j - 1] == s3[i + j - 1])
+                s1[i - 1] != s3[i + j - 1]&& s2[j - 1] == s3[i + j - 1])
                 IL[i][j] = IL[i][j - 1];
             else if (
-                s1[i - 1] == s3[i + j - 1]
-                && s2[j - 1] == s3[i + j - 1])
+                s1[i - 1] == s3[i + j - 1]&& s2[j - 1] == s3[i + j - 1])
                 IL[i][j]= (IL[i - 1][j]|| IL[i][j - 1]);
         }
     }
